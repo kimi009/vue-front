5e0cb56e4b610c0b84bd8c74422e10763ef2a905
chenfuye <chenfuye@vanvy.cn>
2019-10-16 19:03:57 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li v-for="(item,key) in users"
+        :key="key">{{item.name}}</li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/api/users').then(res => {
+      this.users = res.data
+    })
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
